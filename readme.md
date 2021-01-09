c4bc39e58011ac2993425c5ec7c32817dda768c6
Felipe Bonato <felipe23bonato@gmail.com>
2021-01-09 19:22:08 -0300

    iniciando projeto carrinho de compras, criando a home, cart e js

--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 ## Projeto de estudos
 
-- [x] Criar uma pagina com produtos.
-- [x] Criar uma pagina com o carrinho.
-- [x] Pode aumentar o número de produtos do carrinho apertando em mais ou menos.
+- [] Criar uma pagina com produtos(HOME).
+- [] Criar uma pagina com o carrinho.
+- [] Pode aumentar o número de produtos do carrinho apertando em mais ou menos.
---
+++ src/index.js
@@ -0,0 +1,66 @@
+const currentCartItems = 0;
+
+function updateCartNumber(total) {
+    const cartNumberEl = document.getElementById('numero-carrinho')
+    cartNumberEl.textContent = total;
+}
+
+updateCartNumber(currentCartItems);
+
+const cart = [];
+const movies = [
+    {
+        id: 1,
+        name:'De volta para o fututo',
+        price: '13,50'
+    },
+    {
+        id: 2,
+        name: 'Dragon ball Z',
+        price: '10,20'
+    },
+    {
+        id: 3,
+        name: 'A Cabana',
+        price: '14,65'
+    },
+    {
+        id: 4,
+        name: 'Pokemon 2000',
+        price: '5,90'
+    }
+];
+
+const tableMoviesEl = document.querySelector('#table-movies-body');
+
+const movieElements = movies.map((item) => {
+    const tr = document.createElement("tr");
+
+    const colName = document.createElement("td");
+    colName.textContent = item.name;            // o conteudo de texto será item.name
+
+    const colPrice = document.createElement("td");
+    colPrice.textContent = item.price;
+
+    const colAction = document.createElement("td");
+    const buttonAction = document.createElement("button");
+    buttonAction.textContent = 'Adicionar ao carrinho'
+    colAction.appendChild(buttonAction)             // acrescentado botao no 'td'
+    buttonAction.onclick = function () {
+        cart.push(item);
+        updateCartNumber(cart.length)
+        alert("Item adicionado com sucesso");
+
+    }
+
+    
+    tr.appendChild(colName);     //dentro do 'tr' um 'td' com NAME
+    tr.appendChild(colPrice);
+    tr.appendChild(colAction);
+
+    return tr;
+});
+
+movieElements.forEach((createTr) => {
+    tableMoviesEl.appendChild(createTr);
+});
